351ab34a7b68a0f661ed67d9234b3509264e256c
윤성용 <iop017779@gmail.com>
2024-11-03 21:33:11 +0900

    add : fetchHolidays 테스트 코드 추가

--- src/__tests__/unit/easy.fetchHolidays.spec.ts
+++ src/__tests__/unit/easy.fetchHolidays.spec.ts
@@ -1,9 +1,22 @@
 import { fetchHolidays } from '../../apis/fetchHolidays';
+import { expect } from 'vitest';
 
 describe('fetchHolidays', () => {
-  it('주어진 월의 공휴일만 반환한다', () => {});
+  it('주어진 월의 공휴일만 반환한다', () => {
+    const date = new Date('2024-12-25');
 
-  it('공휴일이 없는 월에 대해 빈 객체를 반환한다', () => {});
+    expect(fetchHolidays(date)).toEqual({ '2024-12-25': '크리스마스' });
+  });
 
-  it('여러 공휴일이 있는 월에 대해 모든 공휴일을 반환한다', () => {});
+  it('공휴일이 없는 월에 대해 빈 객체를 반환한다', () => {
+    const date = new Date('2024-11-05');
+
+    expect(fetchHolidays(date)).toEqual({});
+  });
+
+  it('여러 공휴일이 있는 월에 대해 모든 공휴일을 반환한다', () => {
+    const date = new Date('2024-10-01');
+
+    expect(fetchHolidays(date)).toEqual({ '2024-10-03': '개천절', '2024-10-09': '한글날' });
+  });
 });
